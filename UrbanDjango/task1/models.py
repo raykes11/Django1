ee966a4965b3c41ff69c8dd47b7f3ba54dbf0e81
Kondratev Aleksey <raykes4@gmail.com>
2024-09-19 04:19:02 +0300

    task1

--- UrbanDjango/task1/models.py
+++ UrbanDjango/task1/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
 class Buyer(models.Model):
-    name= models.CharField(max_length=30)
-    balance=models.DecimalField(decimal_places=2,max_digits=9)
-    age=models.IntegerField()
+    name = models.CharField(max_length=30)
+    balance = models.DecimalField(decimal_places=2, max_digits=9)
+    age = models.IntegerField()
 
 
 class Game(models.Model):
-    title=models.CharField(max_length=30)
-    cost=models.DecimalField(decimal_places=2,max_digits=9)
-    size=models.DecimalField(decimal_places=2,max_digits=9)
-    description=models.TextField()
-    age_limited=models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer, related_name='games')
+    title = models.CharField(max_length=30)
+    cost = models.DecimalField(decimal_places=2, max_digits=9)
+    size = models.DecimalField(decimal_places=2, max_digits=9)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField('Buyer', related_name='games')
