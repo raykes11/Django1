e7e6d79aa37f3b9e01b414883327f6bb5b86b955
Kondratev Aleksey <raykes4@gmail.com>
2024-09-19 06:59:34 +0300

    task4

--- UrbanDjango/task1/models.py
+++ UrbanDjango/task1/models.py
@@ -7,6 +7,9 @@ class Buyer(models.Model):
     balance = models.DecimalField(decimal_places=2, max_digits=9)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Game(models.Model):
     title = models.CharField(max_length=30)
@@ -15,3 +18,14 @@ class Game(models.Model):
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField('Buyer', related_name='games')
+
+    def __str__(self):
+        return self.title
+
+
+class Info(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
