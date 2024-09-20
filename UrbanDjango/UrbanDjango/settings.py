f3ab0de4f41d478fd59b6b10882dd4fa84bc8d6b
Kondratev Aleksey <raykes4@gmail.com>
2024-09-20 15:56:37 +0300

    task6

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'UrbanDjango.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "datapos",
+        "USER": "postgres",
+        "PASSWORD": "reyasakura1",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
